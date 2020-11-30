bffedbfaf09ea404bc53514e5c8bbd337c20f877
Thomas Bonk <thomas@bonk.dev>
2020-11-30 23:52:36 +0100

    Add styles

--- src/pages/shared/base.ts
+++ src/pages/shared/base.ts
@@ -4,6 +4,7 @@ import Footer from './footer';
 import { addAsset } from '../../utilities/router';
 
 const clientUrl = addAsset('../../../build/client.js');
+const stylesUrl = addAsset('./styles.css');
 
 const Base = (props: { title: string }, ...children: Child[]) => html`
   <!DOCTYPE html>
@@ -12,11 +13,11 @@ const Base = (props: { title: string }, ...children: Child[]) => html`
       <title>${props.title}</title>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link href="/assets/styles.css" rel="stylesheet" />
+      <link href="${stylesUrl}" rel="stylesheet" />
     </head>
-    <body class="">
+    <body>
       ${Header()}
-      <main class="uk-container">
+      <main>
         ${children}
       </main>
       ${Footer()}
--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,4 @@ module.exports = {
       },
     ],
   },
-  watch: !isProduction,
 };

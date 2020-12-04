26e93aee4f0273909d817d417419bfb928790a2c
Thomas Bonk <thomas@bonk.dev>
2020-12-04 16:20:04 +0100

    Import pages automatically

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,8 @@
-import './pages/homepage';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+const PAGES_PATH = './pages/';
+
+readdirSync(join(__dirname, PAGES_PATH))
+  .filter(filename => filename !== 'shared')
+  .forEach(filename => require('./pages/' + filename));
